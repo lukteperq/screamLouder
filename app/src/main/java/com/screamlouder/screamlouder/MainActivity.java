1a4fc12888f55a4d638ac379e6b031550cfda4d8
vbrestad <ninja_brez@hotmail.com>
2016-09-28 18:48:44 +0200

    Created a new instance of MediaRecorder
    It doesn't work, needs fixing.

--- app/src/main/java/com/screamlouder/screamlouder/MainActivity.java
+++ app/src/main/java/com/screamlouder/screamlouder/MainActivity.java
@@ -1,15 +1,27 @@
 package com.screamlouder.screamlouder;
 
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaRecorder recorder = new MediaRecorder();
+    recorder.AudioSource(MediaRecorder.AudioSource.MIC);
+    recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+    recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+    recorder.setOutputFile(PATH_NAME);
+    recorder.prepare();
+    recorder.start();   // Recording is now started
+    recorder.stop();
+    recorder.reset();   // You can reuse the object by going back to setAudioSource() step
+    recorder.release(); // Now the object cannot be reused
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
