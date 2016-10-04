7d08f6ef32bb2c54da6cbf93ac3613747fbd42a5
lukteperq <grimstadoyvind@gmail.com>
2016-10-04 14:15:15 +0200

    WOOP WOOP finally got recording working

--- app/src/main/java/com/screamlouder/screamlouder/MainActivity.java
+++ app/src/main/java/com/screamlouder/screamlouder/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int RECORDER_BPP = 16;
     private static final String AUDIO_RECORDER_FILE_EXT_WAV = ".wav";
-    private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+    private static final String AUDIO_RECORDER_FOLDER = "screamLouder";
     private static final String AUDIO_RECORDER_TEMP_FILE = "record_temp.raw";
     private static final int RECORDER_SAMPLERATE = 44100;
     private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_STEREO;
@@ -49,7 +49,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
-        TextView dbResult = (TextView) findViewById(R.id.dbResult);
+        dbResult = (TextView) findViewById(R.id.dbResult);
+        dbResult.setText("heiosann");
 
         setButtonHandlers();
         enableButtons(false);
@@ -144,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("test", String.valueOf(amplitudeDb));
                 //print to dbResult
 
-                //String res = String.valueOf(amplitudeDb);
+                String res = Double.toString(Math.floor(amplitudeDb));
                 //dbResult.setText(res);
 
 
